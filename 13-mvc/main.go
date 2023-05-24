db8d9fce2dceca85e8f66b82f929e5f4e69ed62b
Fakhry-alta <fakhry.ikhsan.firdaus@gmail.com>
2023-05-24 16:56:47 +0700

    feat: add middleware

--- 13-mvc/main.go
+++ 13-mvc/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"be17/mvc/config"
 	"be17/mvc/routes"
+
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
@@ -10,7 +12,13 @@ func main() {
 	config.InitialMigration()
 
 	e := routes.New()
+	e.Pre(middleware.RemoveTrailingSlash())
+	e.Use(middleware.CORS())
 
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: `[${time_rfc3339}] ${status} ${method} ${host}${path} ${latency_human}` + "\n",
+	}))
+	// middlewares.LogMiddlewares(e)
 	// start server di port
 	e.Logger.Fatal(e.Start(":8080"))
 }
